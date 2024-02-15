8bfe1ddeecd4015520bdbb4672d171a28954401a
Kevin Sanchez <Chilledsonic.ks2@gmail.com>
2024-02-15 13:42:24 -0500

    Completed Exercise 1

--- src/class/user.js
+++ src/class/user.js
@@ -3,13 +3,13 @@ import convertNumber from '../utils/convertNumber';
 export class User {
   name = '';
 
-  income = 0;
+  income = '';
 
   education = '';
 
   constructor(params) {
     this.name = params ? params.name ? params.name : '' : '';
-    this.income = params ? params.income ? convertNumber(params.income) : 0 : 0;
+    this.income = params ? params.income ? convertNumber(params.income) : 0 : '';
     this.education = params ? params.education : '';
   }
 }

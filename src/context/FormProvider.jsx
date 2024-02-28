95a9091dae674054addfe60cae105effa8ef419a
Nagarjunado <161540811+Nagarjunado@users.noreply.github.com>
2024-02-27 21:40:23 -0500

    Update FormProvider.jsx
    Step 2 Fix  Thank you page

--- src/context/FormProvider.jsx
+++ src/context/FormProvider.jsx
@@ -5,6 +5,7 @@ const CreateProviderValue = () => {
   const [formStep, setFormStep] = useState(1);
   const [userData, setUserData] = useState(new User());
   const [isLoading, setIsLoading] = useState(false);
+  const [showThankYou, setShowThankYou] = useState(false); // creation of new state for thank you page
 
   const updateFormStep = (updatedUserData) => {
     if (updatedUserData.name && updatedUserData.income) {
@@ -25,7 +26,8 @@ const CreateProviderValue = () => {
   const saveAndResetData = async () => {
     try {
       await mockSaveData(userData);
-      // ADD THANK YOU PAGE HERE
+      // Show thank you page after saving the data 
+      setShowThankYou(true);
       setUserData(new User());
       setFormStep(1);
       setIsLoading(false);
@@ -43,6 +45,12 @@ const CreateProviderValue = () => {
     setFormStep((prev) => prev - 1);
   };
 
+  // submit function
+  const submitAnother = () => {
+    setShowThankYou(false);
+    setFormStep(1);
+  };
+
   return {
     formStep,
     userData,
@@ -51,6 +59,8 @@ const CreateProviderValue = () => {
     confirmForm,
     backToPreviousStep,
     isLoading,
+    showThankYou, // For showing Thankyou Message
+    submitAnother, // Function for SubmitAnother
   };
 };
 
